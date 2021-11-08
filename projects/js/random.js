a49b3f4ca5b3a62456ababc9216d17a0049f48e1
DHCGamer1055 <dharries2003@gmail.com>
2021-11-08 18:52:25 +0000

    Update random.js

--- projects/js/random.js
+++ projects/js/random.js
@@ -1,9 +1,9 @@
 const output = document.getElementById("num-output");
-const min_input = document.getElementById("num-min-input");
-const max_input = document.getElementById("num-max-input");
 const dice = document.getElementById("number-dice");
 
 dice.onclick = function() {
+    const min_input = document.getElementById("num-min-input");
+    const max_input = document.getElementById("num-max-input");
     var number = Math.floor((Math.random() * max_input.value) + min_input.value);
     output.innerHTML = number;
 }
