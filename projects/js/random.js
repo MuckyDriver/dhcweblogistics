d716c3a07dc432a14584868d2adc870d7b3a5170
DHCGamer1055 <dharries2003@gmail.com>
2021-11-09 19:20:23 +0000

    Update random.js

--- projects/js/random.js
+++ projects/js/random.js
@@ -4,7 +4,7 @@ const input = document.getElementById("num-input");
 
 dice.onclick = function() {
     console.log(input.Value)
-    if (input.Value >= 1) {
+    if (input.Value >= 1 || input.Value == 'undefined') {
         output.innerHTML = '∴';
     } else {
         output.innerHTML = Math.floor((Math.random() * input.value) + 1);
