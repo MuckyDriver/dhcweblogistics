3044d80a298cbd557d8094721f9700a653f1b058
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2022-07-28 23:19:12 +0100

    PlayerLevelModule

---
+++ docs/js/index.js
@@ -0,0 +1,16 @@
+const docsArea = document.getElementById('docs')
+const anchor = document.getElementById('anchor')
+
+let buttons = anchor.getElementsByTagName('a')
+
+for (let i = 0; i < buttons.length; i++) {
+    let btn = buttons[i]
+
+    btn.onclick = function() {
+        let section = docsArea.getElementsByClassName(btn.id)[0]
+
+        if (section) {
+            window.scrollTo(0, section.parentElement.offsetTop - 20)
+        }
+    }
+}
