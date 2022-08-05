b474eb88c9e73bdc3fec3070d5314d42c978a46d
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2022-08-05 11:52:47 +0100

    sadge

--- projects/js/random.js
+++
@@ -1,7 +0,0 @@
-const output = document.getElementById("num-output");
-const dice = document.getElementById("number-dice");
-const input = document.getElementById("num-input");
-
-dice.onclick = function() {
-    output.innerHTML = Math.floor((Math.random() * input.value) + 1);
-}
