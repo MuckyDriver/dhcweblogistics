0c1123933606d3e6299c75f36e951a3bfcf855e5
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2022-09-02 18:14:38 +0100

    rege

--- uploads/js/index.js
+++ uploads/js/index.js
@@ -1,10 +1,10 @@
-var file = new Image();
+let file = new Image();
 let searchPar = window.location.search.split('?')[1]
 
 file.src = 'directories/' + searchPar
 
 if (file.src) {
-    var metaTagEmbed = document.getElementsByTagName('head')[0].getElementsByTagName('meta')[0]
+    let metaTagEmbed = document.getElementsByTagName('head')[0].getElementsByTagName('meta')[0]
     if (metaTagEmbed) { metaTagEmbed.content = file.src + "#a" }
 
     file.style.maxHeight = '100vh';
