8ad111e3fd511469aaa5e7aed89228871c75be06
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2022-09-02 17:53:43 +0100

    Update index.js

--- uploads/js/index.js
+++ uploads/js/index.js
@@ -4,6 +4,11 @@ let searchPar = window.location.search.split('?')[1]
 file.src = 'directories/' + searchPar
 
 if (file.src) {
+    var metaTagEmbed = document.createElement('meta')
+    metaTagEmbed.content = file.src
+    metaTagEmbed.setAttribute('property', 'og:image');
+    document.head.insertBefore(metaTagEmbed, document.head.firstChild)
+
     file.style.maxHeight = '100vh';
     file.style.maxWidth = '100%';
     file.style.transform = 'translateY(-50%) translateX(-50%)';
@@ -12,11 +17,6 @@ if (file.src) {
     file.style.left = '50%';
     file.alt = searchPar
 
-    var metaTagEmbed = document.createElement('meta')
-    metaTagEmbed.content = file.src
-    metaTagEmbed.setAttribute('property', 'og:image');
-
-    document.head.appendChild(metaTagEmbed)
     document.body.appendChild(file)
     document.title = searchPar
 }
