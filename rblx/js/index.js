4140ef6234b817a2e3ff56bfc3555e8669208556
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2022-09-21 22:08:56 +0100

    $copy + $copy-info | Added

--- rblx/js/index.js
+++ rblx/js/index.js
@@ -237,16 +237,13 @@ window.onload = function() {
 }
 
 /* Copyright Toggle Information */
-const copyright_element = document.getElementById('$copyright')
-const copyrightInfo_element = document.getElementById('$copyright-info')
+const copyright_element = document.getElementById('$copy')
+const copyrightInfo_element = document.getElementById('$copy-info')
 let copyright_enabled = false
 
 copyright_element.onclick = function() {
-    if (copyrightInfo_element) {
-        copyrightInfo_element.classList.add('hidden')
-    } else {
-        copyrightInfo_element.classList.remove('hidden')
-    }
+    if (copyright_enabled) { copyrightInfo_element.classList.add('hidden')
+    } else { copyrightInfo_element.classList.remove('hidden') }
 
-    copyright_enabled != copyright_enabled
+    copyright_enabled = !copyright_enabled
 }
