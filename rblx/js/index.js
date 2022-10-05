0ec3f3e7caf218c5fed60a519f107ef2d1adb949
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2022-10-05 21:27:50 +0100

    Copyright sub-domain notice on all pages

--- rblx/js/index.js
+++ rblx/js/index.js
@@ -216,15 +216,3 @@ window.onload = function() {
         })()
     }
 }
-
-/* Copyright Toggle Information */
-const copyright_element = document.getElementById('$copy')
-const copyrightInfo_element = document.getElementById('$copy-info')
-let copyright_enabled = false
-
-copyright_element.onclick = function() {
-    if (copyright_enabled) { copyrightInfo_element.classList.add('hidden')
-    } else { copyrightInfo_element.classList.remove('hidden') }
-
-    copyright_enabled = !copyright_enabled
-}
