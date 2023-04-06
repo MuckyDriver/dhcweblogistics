bca6763ad511e184d82dfa6517fe8c14cf6e6b1c
DHCGamer1055 <dharries2003@gmail.com>
2023-04-06 23:52:27 +0100

    404 2

--- src/404.js
+++
@@ -1,7 +0,0 @@
-const confirmed = confirm('Error 404: Page not found, the url does not exist on this domain. Close this tab or click OK to return to the domains main page.')
-    
-if (confirmed) {
-    window.location.replace('/'); 
-}
-
-document.body.style.backgroundColor = 'black';
