3a4daa4efbc56cf13edef2b0c276cd310d7239a3
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2023-04-09 11:21:47 +0100

    grrr

--- js/index.js
+++ js/index.js
@@ -6,14 +6,14 @@ let open = false;
 
 mobileMenuBtn.onclick = function() {
     if (open == false) {
-        mobileMenu.classList.remove('closed'); // Removing 'closed' class will transition show the mobile menu.
+        mobileMenu.classList.add('open'); // Removing 'closed' class will transition show the mobile menu.
         open = true;
     }
 }
 
 mobileMenuCloseBtn.onclick = function() {
     if (open == true) {
-        mobileMenu.classList.add('closed'); // Adding 'closed' class will transition hide the mobile menu.
+        mobileMenu.classList.remove('open'); // Adding 'closed' class will transition hide the mobile menu.
         open = false;
     }
 }
