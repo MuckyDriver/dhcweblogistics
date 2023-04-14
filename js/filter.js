746aaab683415414c3ef7cdb8327d2abfd10c391
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2023-04-14 21:07:05 +0100

    Projects filter by category

---
+++ js/filter.js
@@ -0,0 +1,23 @@
+// Function which searches for the keyword relating to chosen category
+const projectItems = document.getElementsByClassName('project-item')
+
+function applyChanges(category) {
+    for (let i = 0; i < projectItems.length; i++) {
+        const projectItem = projectItems[i];
+        let relatedCategories = projectItem.getAttribute('category').split(" ")
+
+        if (relatedCategories.includes(category)) {
+            projectItem.style.display = 'grid';
+        } else {
+            projectItem.style.display = 'none';
+        }
+    }
+}
+
+// Getting globals needed for filter script
+const filterSelect = document.getElementById('filter')
+
+filterSelect.addEventListener('change', (event) => {
+    let categorySelected = event.target.value;
+    applyChanges(categorySelected)
+})
