89b0202ce9501c55c617331135e21145f0eb99e7
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2023-04-18 00:46:23 +0100

    geewiz

--- js/index.js
+++ js/index.js
@@ -22,7 +22,8 @@ mobileMenuCloseBtn.onclick = function() {
 const projectList = document.getElementById('project-list');
 const projectUrls = {
     [3]: "https://www.roblox.com/games/12633113230/",
-    [4]: "https://muckydriver.net/"
+    [4]: "https://muckydriver.net/",
+    [5]: "https://www.roblox.com/games/12468321078/"
 }
 
 if (projectList) {
