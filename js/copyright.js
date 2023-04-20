8817e39cba3810d9283a04964e1b2c22dbbe9f07
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2023-04-20 23:41:30 +0100

    copyright symbol

--- js/copyright.js
+++ js/copyright.js
@@ -1,11 +1,41 @@
+/* Copyright Section Creation */
+const copyrightElement = document.createElement('div');
+copyrightElement.classList.add('copyright', 'opaque');
+
+const copySymbolDiv = document.createElement('div');
+copySymbolDiv.innerHTML = "<span>© </span>" + (new Date().getFullYear());
+
+const copyInfoDiv = document.createElement('div');
+copyInfoDiv.classList = "info hidden";
+copyInfoDiv.innerHTML = "Webpage copyright under dhcweblogistics.net domain.";
+
+copyrightElement.appendChild(copySymbolDiv);
+copyrightElement.appendChild(copyInfoDiv);
+document.body.appendChild(copyrightElement);
+
 /* Copyright Toggle Information */
-const copyright_element = document.getElementById('$copy')
-const copyrightInfo_element = document.getElementById('$copy-info')
-let copyright_enabled = false
+let copyrightEnabled = false
 
-copyright_element.onclick = function() {
-    if (copyright_enabled) { copyrightInfo_element.classList.add('hidden')
-    } else { copyrightInfo_element.classList.remove('hidden') }
+copyrightElement.onclick = function() {
+    if (copyrightEnabled) { copyInfoDiv.classList.add('hidden')
+    } else { copyInfoDiv.classList.remove('hidden') }
 
-    copyright_enabled = !copyright_enabled
+    copyrightEnabled = !copyrightEnabled
+}
+
+/* Hitbox for checking if mouse is in the same area as the copyright button by a certain amount */
+const newHitbox = document.createElement('div');
+newHitbox.classList.add("hitbox-copy");
+newHitbox.style.top = copyrightElement.offsetTop + "px";
+newHitbox.style.left = copyrightElement.offsetLeft + "px";
+
+document.body.appendChild(newHitbox);
+
+// Actual event checking whether something like a mouse hovers over.
+newHitbox.onmouseenter = function() {
+    copyrightElement.classList.remove('opaque');
+}
+
+newHitbox.onmouseleave = function() {
+    copyrightElement.classList.add('opaque')
 }
