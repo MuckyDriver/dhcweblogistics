5f41c1ec833071c005e3e1efc677236e358149d0
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2023-05-04 23:42:32 +0100

    this is coming soon ofc

---
+++ js/dashboard-nav.js
@@ -0,0 +1,71 @@
+// Dashboard navigational elements
+const navElements = document.getElementsByClassName('dashboard-navigation');
+const dashPages = document.getElementsByClassName('dash-page');
+
+// Toggle Collapsing function
+const toggleCollapse = document.getElementById('collapse_uncollapse')
+let collapsed = true;
+
+toggleCollapse.onclick = function() {
+    if (collapsed == true) {
+        toggleCollapse.parentElement.classList.remove('closed')
+    } else {
+        toggleCollapse.parentElement.classList.add('closed')
+    }
+
+    collapsed = !collapsed
+}
+
+// Functions
+function togglePage(reference) {
+    for (let i = 0; i < dashPages.length; i++) {
+        const dashPage = dashPages[i];
+        const pageReference = dashPage.getAttribute('data-pageReference');
+
+        if (pageReference == reference) {
+            dashPage.style.display = 'block';
+        } else {
+            dashPage.style.display = 'none';
+        }
+    }
+}
+
+// Removing styling, like all the active buttons as new place is active now.
+function resetButtonStyling() {
+    for (let i = 0; i < navElements.length; i++) {
+        const buttonElements = navElements[i].getElementsByTagName('button');
+
+        for (let i = 0; i < buttonElements.length; i++) {
+            buttonElements[i].classList.remove('active');
+        }
+    }
+
+    if (collapsed == false) {
+        toggleCollapse.parentElement.classList.add('closed')
+        collapsed = !collapsed
+    };
+}
+
+// Looping through all navElements
+for (let i = 0; i < navElements.length; i++) {
+    const navElement = navElements[i];
+    const buttonElements = navElement.getElementsByTagName('button');
+
+
+    for (let i = 0; i < buttonElements.length; i++) {
+        const button = buttonElements[i];
+        const reference = button.getAttribute('data-reference');
+
+        function action() {
+            resetButtonStyling(); button.classList.add('active')
+            togglePage(reference);
+        }
+
+        button.addEventListener("click", () => { action() })
+
+        // In some cases a keyboard is used to toggle through the pages, this allows it to work for those people.
+        button.addEventListener("keypress", (event) => {
+            if (event.key === "Enter") { action() }
+        });
+    }
+}
